c8fdc92e4d0d5a614f0125c04eb4ee27c3a81cea
crescens <crescens.techstars@gmail.com>
2014-01-07 11:40:06 +0800

    Signed-off-by: crescens <crescens.techstars@gmail.com>

---
+++ SuperDB/SuperDB/BIDHeroListController.h
@@ -0,0 +1,13 @@
+//
+//  BIDHeroListController.h
+//  SuperDB
+//
+//  Created by Crescens Techstars on 1/6/14.
+//  Copyright (c) 2014 Crescens Techstars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDHeroListController : UITableViewController
+
+@end
