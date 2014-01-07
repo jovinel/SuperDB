ed82ca50e8bcd602f3071ede8388164470588c6a
crescens <crescens.techstars@gmail.com>
2014-01-07 11:43:10 +0800

    Implemented codes to create new table data in tableview controller and saves to the core data.
    Signed-off-by: crescens <crescens.techstars@gmail.com>

--- SuperDB/SuperDB/BIDHeroListController.m
+++ SuperDB/SuperDB/BIDHeroListController.m
@@ -7,21 +7,22 @@
 //
 
 #import "BIDHeroListController.h"
+#import "BIDAppDelegate.h"
+
+#define kSelectedTabDefaultsKey @"Selected Tab"
+
+enum {
+    kByName,
+    kBySecretIdentity,
+};
 
 @interface BIDHeroListController ()
-
+@property (nonatomic, strong, readonly) NSFetchedResultsController *fetchedResultsController;
 @end
 
 @implementation BIDHeroListController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+@synthesize fetchedResultsController = _fetchedResultsController;
 
 - (void)viewDidLoad
 {
@@ -31,7 +32,21 @@
     // self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+     self.navigationItem.leftBarButtonItem = self.editButtonItem;
+    
+    // Select the Tab Bar button
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSInteger selectedTab = [defaults integerForKey:kSelectedTabDefaultsKey];
+    UITabBarItem *item = [self.heroTabBar.items objectAtIndex:selectedTab];
+    [self.heroTabBar setSelectedItem:item];
+    
+    // Fetch any existing entities
+    NSError *error = nil;
+    if (![[self fetchedResultsController] performFetch:&error]) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error loadin data", @"Error loading date") message:[NSString stringWithFormat:NSLocalizedString(@"Error was: %@, quitting", @"Error was: %@, quitting"), [error localizedDescription]] delegate:self cancelButtonTitle:NSLocalizedString(@"Aw, Nuts", @"Aw, Nuts") otherButtonTitles: nil];
+        
+        [alert show];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,25 +59,35 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return [[self.fetchedResultsController sections] count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    id<NSFetchedResultsSectionInfo>sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
+    return [sectionInfo numberOfObjects];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    static NSString *CellIdentifier = @"HeroListCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
     // Configure the cell...
-    
+    NSManagedObject *aHero = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    NSInteger tab = [self.heroTabBar.items indexOfObject:self.heroTabBar.selectedItem];
+    switch (tab) {
+        case kByName:
+            cell.textLabel.text = [aHero valueForKey:@"name"];
+            cell.detailTextLabel.text = [aHero valueForKey:@"secretIdentity"];
+            break;
+        case kBySecretIdentity:
+            cell.textLabel.text = [aHero valueForKey:@"secretIdentity"];
+            cell.detailTextLabel.text = [aHero valueForKey:@"name"];
+            break;
+    }
     return cell;
 }
 
@@ -75,19 +100,25 @@
 }
 */
 
-/*
+
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    NSManagedObjectContext *managedObjectContext = [self.fetchedResultsController managedObjectContext];
+    
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+        [managedObjectContext deleteObject:[self.fetchedResultsController objectAtIndexPath:indexPath]];
+        
+        NSError *error;
+        if (![managedObjectContext save:&error]) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error saving entity", @"Error saving entity") message: [NSString stringWithFormat:NSLocalizedString(@"Error was: %@, quitting.", @"Error was: %@, quitting"), [error localizedDescription]] delegate:self cancelButtonTitle:NSLocalizedString(@"Aw, Nuts", @"Aw, Nuts") otherButtonTitles:nil];
+            
+            [alert show];
+        }
+    }
 }
-*/
+
 
 /*
 // Override to support rearranging the table view.
@@ -116,5 +147,136 @@
 }
 
  */
+
+#pragma mark - UITabBarDelegate Methods
+-(void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSUInteger tabIndex = [tabBar.items indexOfObject:item];
+    [defaults setInteger:tabIndex forKey:kSelectedTabDefaultsKey];
+    
+    [NSFetchedResultsController deleteCacheWithName:@"Hero"];
+    _fetchedResultsController.delegate = nil;
+    _fetchedResultsController = nil;
+    
+    NSError *error;
+    if (![self.fetchedResultsController performFetch:&error]) {
+        NSLog(@"Error performing fetch: %@", [error localizedDescription]);
+    }
+    
+    [self.heroTableView reloadData];
+}
+
+#pragma mark - FetchedResultsController Property
+- (NSFetchedResultsController *)fetchedResultsController {
+    if (_fetchedResultsController != nil) {
+        return _fetchedResultsController;
+    }
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    BIDAppDelegate *appDelegate = (BIDAppDelegate *)[[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *managedObjectContext = [appDelegate managedObjectContext];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Hero" inManagedObjectContext:managedObjectContext];
+    
+    [fetchRequest setEntity:entity];
+    [fetchRequest setFetchBatchSize:20];
+    
+    NSUInteger tabIndex = [self.heroTabBar.items indexOfObject:self.heroTabBar.selectedItem];
+    if (tabIndex == NSNotFound) {
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        tabIndex = [defaults integerForKey:kSelectedTabDefaultsKey];
+    }
+    
+    NSString *sectionKey = nil;
+    switch (tabIndex) {
+            // Notice that the kByName and kBySecretIdentity Code are nearly identical.
+            // A refactoring opportunity?
+        case kByName: {
+            NSSortDescriptor *sortDescriptor1 = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
+            NSSortDescriptor *sortDescriptor2 = [[NSSortDescriptor alloc] initWithKey:@"secretIdentity" ascending:YES];
+            NSArray *sortDescriptors = [[NSArray alloc] initWithObjects:sortDescriptor1, sortDescriptor2, nil];
+            [fetchRequest setSortDescriptors:sortDescriptors];
+            sectionKey = @"name";
+            break;
+        }
+        case kBySecretIdentity: {
+            NSSortDescriptor *sortDescriptor1 = [[NSSortDescriptor alloc] initWithKey:@"secretIdentity" ascending:YES];
+            NSSortDescriptor *sortDescriptor2 = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
+            NSArray *sortDescriptors = [[NSArray alloc] initWithObjects:sortDescriptor1, sortDescriptor2, nil];
+            [fetchRequest setSortDescriptors:sortDescriptors];
+            sectionKey = @"secretIdentity";
+            break;
+        }
+    }
+    
+    _fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:managedObjectContext sectionNameKeyPath:sectionKey cacheName:@"Hero"];
+    
+    _fetchedResultsController.delegate = self;
+    
+    return _fetchedResultsController;
+}
+
+#pragma mark - NSFetchedResultsControllerDelegate Methods
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller
+{
+    [self.heroTableView beginUpdates];
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller
+{
+    [self.heroTableView endUpdates];
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id<NSFetchedResultsSectionInfo>)sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type
+{
+    switch (type) {
+        case NSFetchedResultsChangeInsert:
+            [self.heroTableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        case NSFetchedResultsChangeDelete:
+            [self.heroTableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath
+{
+    switch (type) {
+        case NSFetchedResultsChangeInsert:
+            [self.heroTableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        case NSFetchedResultsChangeDelete:
+            [self.heroTableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        case NSFetchedResultsChangeUpdate:
+        case NSFetchedResultsChangeMove:
+            break;
+    }
+}
+
+#pragma mark - UIAlertViewDelegate Methods
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    exit(-1);
+}
+
+- (IBAction)addHero:(id)sender {
+    NSManagedObjectContext *managedObjectContext = [self.fetchedResultsController managedObjectContext];
+    NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
+    [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:managedObjectContext];
+    
+    NSError *error = nil;
+    if (![managedObjectContext save:&error]) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error saving entity", @"Error saving entity") message:[NSString stringWithFormat:NSLocalizedString(@"Error was: %@, quitting.", @"Error was: %@, quitting."), [error localizedDescription]] delegate:self cancelButtonTitle:NSLocalizedString(@"Aw, Nuts", @"Aw, Nuts") otherButtonTitles:nil];
+        
+        [alert show];
+    }
+}
+
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated
+{
+    [super setEditing:editing animated:animated];
+    self.addButton.enabled = !editing;
+    [self.heroTableView setEditing:editing animated:animated];
+}
 
 @end
